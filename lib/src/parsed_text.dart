b4af4422df7348a54c2eb50e6b1bb811358b9b1a
WaiYanSoeGithubMyanmar <waiyansoe4421@gmail.com>
2021-10-13 14:55:53 +0630

    bug fix

--- lib/src/parsed_text.dart
+++ lib/src/parsed_text.dart
@@ -153,9 +153,6 @@ class ParsedText extends StatelessWidget {
             Map<String, String> result =
                 mapping.renderText!(str: matchText, pattern: pattern);
 
-            print("mentionme>> $mentionme");
-            print("matchText>> $matchText");
-
             if (mentionme == matchText) {
               widget = TextSpan(
                 text: "${result['display']}",
